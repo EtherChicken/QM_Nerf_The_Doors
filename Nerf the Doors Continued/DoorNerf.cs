39c4698bea106eac3d5ae70eb937ca193d6abd30
E Van Horn <ethanvanhorn@gmail.com>
2025-02-16 15:15:46 -0600

    Initial Commit

---
+++ Nerf the Doors Continued/DoorNerf.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using MGSC;
+using UnityEngine;
+
+namespace Nerf_the_Doors_Continued
+{
+    [HarmonyPatch(typeof(PlayerInteractionSystem),nameof(PlayerInteractionSystem.EndPlayerTurn))]
+    class DoorPatch
+    {
+        public static bool Prefix(Creatures creatures, PlayerEndTurnContext context = PlayerEndTurnContext.None)
+        {
+            // Not sure if you can do it with a postfix, this is the easiest way I can tell.
+            if (context == PlayerEndTurnContext.MapObstacleInteraction)
+            {
+                if (creatures.Player.FreeInteractObstacles)
+                {
+                    return false;
+                }
+                if (creatures.Player.ActionPointsLeft == 1)
+                {
+                    return false;
+                    
+                }
+                creatures.Player.AnyActionProcessedFlag = true;
+            }
+            if (context == PlayerEndTurnContext.InventoryInteraction)
+                creatures.Player.AnyActionProcessedFlag = true;
+            else
+                creatures.Player.IsProcessing = false;
+            return false;
+        } 
+    }
+    
+}
---
+++ Nerf the Doors Continued/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.CodeDom;
+using System.Net.Mime;
+using Unity;
+// using System.Runtime.Serialization.Json;
+// using SimpleJSON;
+
+namespace Nerf_the_Doors_Continued
+{
+    public class Plugin
+    {
+        [Hook(ModHookType.BeforeBootstrap)]
+        public static void Bootstrap(IModContext context)
+        {
+            Harmony harmony = new Harmony("Nerf_the_Doors_Continued");
+            harmony.PatchAll();
+            Debug.Log((object)"New Door Nerf loaded :) \n \n \n");
+        }
+    }
+}
+
